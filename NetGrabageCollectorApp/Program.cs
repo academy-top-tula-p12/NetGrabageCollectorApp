e1984dfff40fc3b8add07bf9ef84773fe2df6d19
maxefimov <efimov.top.academy@gmail.com>
2024-01-18 19:43:00 +0300

    Добавьте файлы проекта.

---
+++ NetGrabageCollectorApp/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine(GC.MaxGeneration);
+
+Console.WriteLine($"Total using memory: {GC.GetTotalMemory(false)}");
+
+using(User user = new())
+{
+    Console.WriteLine($"Total using memory: {GC.GetTotalMemory(false)}");
+}
+
+
+class Employee
+{
+    public string Name { set; get; }
+    public int Age { set; get; }
+}
+
+class User : IDisposable
+{
+    public string Name { set; get; }
+    public int Age { set; get; }
+    public int[] array = new int[100000];
+
+    public void Dispose()
+    {
+        Console.WriteLine("Dispose user");
+    }
+}
